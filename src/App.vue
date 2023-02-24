f60ea8fe5d44ee026e0565dba109eb4e34b6e17f
Fufeck <fabien.tafforeau@gmail.com>
2023-02-24 18:31:43 +0100

    start commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+  #app {
+    position: relative;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    overflow: scroll;
+  }
+</style>
